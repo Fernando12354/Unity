2b746e45ae315237f2168d8921282815f3dbd7e6
OctavioAGarcia <153787158+OctavioAGarcia@users.noreply.github.com>
2024-09-08 19:39:48 -0600

    Cambios a escena Tienda
    Se suben cambios a la escena Tienda

--- SurviveThePandemic-main/SurviveThePandemic-main/SurviveThePandemic/Assets/Guia/objFinal.cs
+++ SurviveThePandemic-main/SurviveThePandemic-main/SurviveThePandemic/Assets/Guia/objFinal.cs
@@ -7,12 +7,17 @@ public class objFinal : MonoBehaviour
     public GameObject panelMision;
     public GameObject panelAnterior;
     public GameObject elemento;
+   public GameObject ZonaDeDialogo;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             panelAnterior.SetActive(false);
             panelMision.SetActive(true);
+            //Linea de codigo para activar el dialogo
+            if(ZonaDeDialogo!=null){
+                ZonaDeDialogo.SetActive(true);
+            }
             Destroy(elemento);
         }
     }
