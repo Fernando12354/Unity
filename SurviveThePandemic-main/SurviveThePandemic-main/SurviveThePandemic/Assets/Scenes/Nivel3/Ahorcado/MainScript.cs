1f72567561f13fcd6ca3a097a2aad18a674722ec
Fernando12354 <165752699+Fernando12354@users.noreply.github.com>
2024-10-08 23:12:58 -0600

    ahorcado
    juego y mas cosas

---
+++ SurviveThePandemic-main/SurviveThePandemic-main/SurviveThePandemic/Assets/Scenes/Nivel3/Ahorcado/MainScript.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Linq;
+using System.Text;
+using UnityEngine.EventSystems;
+using SimpleJSON;
+
+public class MainScript : MonoBehaviour
+{
+    public Text QuestionText, DashesText, ResultsText;
+    public Image HangmanImage, FinalImage;
+    public Sprite[] HangmanSprites;
+    public Sprite WinSprite, LoseSprite;
+
+    public GameObject MainDialogue, FinalDialogue, Escenario, Ahorcado;
+
+    private int currentHangmanSprite = 0;
+    private const int TOTAL_HANGMAN_SPRITES = 8;
+    private const char PLACEHOLDER = '*';
+
+    private Dictionary<string, string> gameDict;
+    private string answer, userInput;
+
+    void Start()
+    {
+        gameDict = new Dictionary<string, string>();
+        LoadDictionary("FruitsDictionary", gameDict);
+        LoadDictionary("SportsDictionary", gameDict);
+        PickRandomQuestion();
+    }
+
+    public void OnRestartClicked()
+    {
+        MainDialogue.SetActive(true);
+        FinalDialogue.SetActive(false);
+        currentHangmanSprite = 0;
+        HangmanImage.sprite = HangmanSprites[currentHangmanSprite];
+        PickRandomQuestion();
+    }
+
+    public void OnGuessSubmitted(Button button)
+{
+    // Agregamos una línea de depuración para saber si se ha pulsado el botón
+    Debug.Log("Botón pulsado: " + button.GetComponentInChildren<Text>().text);
+
+    // Obtener la letra del botón
+    char letter = button.GetComponentInChildren<Text>().text.ToCharArray()[0];
+    
+    // Verificamos si la respuesta contiene la letra
+    if (answer.Contains(letter))
+    {
+        UpdateAnswerText(letter);
+        if (CheckWinCondition())
+        {
+            Debug.Log("Has ganado el juego!");
+            ShowFinalDialogue(true);
+        }
+    }
+    else
+    {
+        if (CheckLoseCondition())
+        {
+            Debug.Log("Has perdido el juego");
+            ShowFinalDialogue(false);
+        }
+        else
+        {
+            DrawNextHangmanPart();
+        }
+    }
+}
+
+      // Método para activar y desactivar objetos
+    public void Continuar()
+    {
+        Ahorcado.SetActive(false);
+       Escenario.SetActive(true);
+       
+    }
+
+    private void PickRandomQuestion()
+    {
+        int randInt = Random.Range(0, gameDict.Count);
+        QuestionText.text = gameDict.ElementAt(randInt).Key;
+        answer = gameDict.ElementAt(randInt).Value.ToUpper();
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < answer.Length; i++) { sb.Append(PLACEHOLDER); }
+        DashesText.text = sb.ToString();
+        userInput = sb.ToString();
+        Debug.Log("Answer: " + answer);
+    }
+
+    private void LoadDictionary(string dictFileName, Dictionary<string, string> outputDict)
+    {
+        TextAsset ta = Resources.Load(dictFileName) as TextAsset;
+        JSONObject jsonObj = (JSONObject)JSON.Parse(ta.text);
+
+        foreach (var keyValuePair in jsonObj)
+        {
+            string key = keyValuePair.Key;
+            string value = keyValuePair.Value;
+            outputDict[key] = value;
+        }
+    }
+
+    private void UpdateAnswerText(char letter)
+    {
+        char[] userInputArray = userInput.ToCharArray();
+        for (int i = 0; i < answer.Length; i++)
+        {
+            if (userInputArray[i] != PLACEHOLDER) { continue; } // already guessed
+            if (answer[i] == letter) { userInputArray[i] = letter; }
+        }
+        userInput = new string(userInputArray);
+        DashesText.text = userInput;
+    }
+
+    private void DrawNextHangmanPart()
+    {
+        currentHangmanSprite = ++currentHangmanSprite % TOTAL_HANGMAN_SPRITES;
+        HangmanImage.sprite = HangmanSprites[currentHangmanSprite];
+    }
+
+    private bool CheckWinCondition() { return answer.Equals(userInput); }
+    private bool CheckLoseCondition() { return currentHangmanSprite == TOTAL_HANGMAN_SPRITES - 1; }
+
+    private void ShowFinalDialogue(bool win)
+    {
+        MainDialogue.SetActive(false);
+        FinalDialogue.SetActive(true);
+        FinalImage.sprite = win ? WinSprite : LoseSprite;
+        ResultsText.text = win ? "Victoria !" : "Derrota !!!";
+    }
+
+  
+}
--- SurviveThePandemic-main/SurviveThePandemic-main/SurviveThePandemic/Assets/Scripts/ScriptNv3/IniciarJuegoZonaAhorcado.cs
+++ SurviveThePandemic-main/SurviveThePandemic-main/SurviveThePandemic/Assets/Scripts/ScriptNv3/IniciarJuegoZonaAhorcado.cs
@@ -4,30 +4,22 @@ using UnityEngine;
 
 public class IniciarJuegoZonaAhorcado : MonoBehaviour
 {
-    private bool juegoIniciado = false; // Para evitar múltiples inicios del juego
+    public GameObject ahorcado;
+    public GameObject escenario;
 
     // Método que se llama cuando otro objeto entra en el trigger
     private void OnTriggerEnter(Collider other)
     {
-        // Verifica si el objeto que ha colisionado tiene el tag "Player" y si el juego no ha sido iniciado aún
-        if (other.CompareTag("Player") && !juegoIniciado)
+        // Verifica si el objeto que ha colisionado tiene el tag "Player"
+        if (other.CompareTag("Player"))
         {
             Debug.Log("Jugador ha entrado en la zona de activación"); // Imprime en la consola cuando el jugador entra en la zona
-            
-            juegoIniciado = true; // Marca que el juego ha comenzado para evitar reinicios
-            AhorcadoGame ahorcadoGame = FindObjectOfType<AhorcadoGame>(); // Busca el script AhorcadoGame en la escena
-            
-            // Si se encuentra el script AhorcadoGame, inicia el juego automáticamente
-            if (ahorcadoGame != null)
-            {
-                ahorcadoGame.IniciarJuegoAutomáticamente(); // Llama al método para iniciar el juego
-                ahorcadoGame.canvasJuego.gameObject.SetActive(true);
-                Debug.Log("El juego de ahorcado ha comenzado."); // Confirma que el juego se ha iniciado
-            }
-            else
-            {
-                Debug.LogWarning("No se encontró el script AhorcadoGame en la escena."); // Si no se encuentra el script, lanza una advertencia
-            }
+            escenario.SetActive(false);
+            ahorcado.SetActive(true);
+
+            // Desactiva el objeto donde está adjuntado este script
+            gameObject.SetActive(false);
         }
     }
 }
+
